89eaca6813702410a8f6abfc8dcce8facea8a0bf
sajltaha <sajlatyer77@gmaill.com>
2025-10-30 15:06:45 +0500

    feat(asik-6): strategy pattern project

--- src/main/RideSystemDemo.java
+++ src/main/RideSystemDemo.java
@@ -13,13 +13,17 @@ public class RideSystemDemo {
         System.out.println("========================================");
         System.out.println("RIDE BOOKING SYSTEM");
         System.out.println("========================================");
+
         RideRequest request = new RideRequest(15, 2, "Talgat Sailaubekov");
+
         System.out.println("\nExample 1: Pool Ride");
         request.setRideStrategy(poolRide);
         request.processRequest();
+
         System.out.println("\nExample 2: Economy Ride");
         request.setRideStrategy(economyRide);
         request.processRequest();
+
         System.out.println("\nExample 3: Premium Ride");
         request.setRideStrategy(premiumRide);
         request.processRequest();
