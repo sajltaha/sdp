54ea47e56873a37a95dd5ef2db943af7ab13988a
sajltaha <sajlatyer77@gmaill.com>
2025-10-30 15:05:22 +0500

    feat(asik-6): strategy pattern project

--- src/main/RideSystemDemo.java
+++ src/main/RideSystemDemo.java
@@ -2,21 +2,26 @@ package main;
 import main.concrete_strategies.EconomyRide;
 import main.concrete_strategies.PoolRide;
 import main.concrete_strategies.PremiumRide;
+import main.interfaces.IRideStrategy;
 
 public class RideSystemDemo {
     public static void main(String[] args) {
+        IRideStrategy poolRide = new PoolRide();
+        IRideStrategy economyRide = new EconomyRide();
+        IRideStrategy premiumRide = new PremiumRide();
+
         System.out.println("========================================");
         System.out.println("RIDE BOOKING SYSTEM");
         System.out.println("========================================");
         RideRequest request = new RideRequest(15, 2, "Talgat Sailaubekov");
         System.out.println("\nExample 1: Pool Ride");
-        request.setRideStrategy(new PoolRide());
+        request.setRideStrategy(poolRide);
         request.processRequest();
         System.out.println("\nExample 2: Economy Ride");
-        request.setRideStrategy(new EconomyRide());
+        request.setRideStrategy(economyRide);
         request.processRequest();
         System.out.println("\nExample 3: Premium Ride");
-        request.setRideStrategy(new PremiumRide());
+        request.setRideStrategy(premiumRide);
         request.processRequest();
     }
 }
