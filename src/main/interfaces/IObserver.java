e5903888b81bc159d5fabcc225b83a841b6b1b04
sajltaha <sajlatyer77@gmaill.com>
2025-11-10 10:35:32 +0500

    feat(asik-8): visitor pattern project

--- src/main/interfaces/IObserver.java
+++
@@ -1,5 +0,0 @@
-package main.interfaces;
-
-public interface IObserver {
-    void update(float temperature, float humidity, float pressure);
-}
--- src/main/interfaces/ISubject.java
+++
@@ -1,7 +0,0 @@
-package main.interfaces;
-
-public interface ISubject {
-    void registerObserver(IObserver o);
-    void removeObserver(IObserver o);
-    void notifyObservers();
-}
